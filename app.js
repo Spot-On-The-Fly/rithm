3794b0fe49e955474865eacb70e89a186fb5592f
GPadley <george.r.padley@gmail.com>
2017-02-04 14:27:50 +0000

    -a

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 var george = require('./georgeinfo.json');
+console.log("George")
 for(var i = 0; i < 10; i++){
 console.log(george.items[i].artists[0].name + " " + george.items[i].name);
 }
-// var user = new object{
-//   username : 'username';
-//   date : new Date();
-//
-// }
-// var jack = new user('TrebleStick');
-//
-//
-// console.log(jack.username jack.date);
+var jack = require('./jackinfo.json')
+console.log("jack")
+for(var i = 0; i < 10; i++){
+console.log(jack.items[i].artists[0].name + " " + jack.items[i].name);
+}
