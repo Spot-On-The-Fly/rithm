b80f4aaee3956146f614be0ba2b4ab8ab5b7d3c5
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-02-05 07:07:09 +0000

    -a

--- fetch.js
+++ fetch.js
@@ -12,14 +12,14 @@ spotifyApi.getUser('BQA-VMlANGOMpR09g5nTMCTYtd5R78bpxueV08E8L5z20zge34gI8ujH8933
   });
 
 //get top tracks
-spotifyApi.getMyTopTracks('tracks', 'BQA-VMlANGOMpR09g5nTMCTYtd5R78bpxueV08E8L5z20zge34gI8ujH8933Y2tsz7M2TDPyKf1oPp3wWsghIu2SIfyQEXAeOJY8U2AeqX6CHWr1ji7OE17mX247b_IFi9kdGQS_aLdKSTLhv4KRTQaxQaOWo1Jpv2o', trackcount, 0, 'medium_term').then(function(data) {
+spotifyApi.getMyTopTracks('tracks', 'BQA-VMlANGOMpR09g5nTMCTYtd5R78bpxueV08E8L5z20zge34gI8ujH8933Y2tsz7M2TDPyKf1oPp3wWsghIu2SIfyQEXAeOJY8U2AeqX6CHWr1ji7OE17mX247b_IFi9kdGQS_aLdKSTLhv4KRTQaxQaOWo1Jpv2o', trackcount, 0, 'medium_term', 'GB').then(function(data) {
         console.log('TOP TRACKS', data.body);
         }, function(err) {
             console.log('Something went wrong!', err);
         });
 
 //get top artists
-spotifyApi.getMyTopArtists('artists', 'BQA-VMlANGOMpR09g5nTMCTYtd5R78bpxueV08E8L5z20zge34gI8ujH8933Y2tsz7M2TDPyKf1oPp3wWsghIu2SIfyQEXAeOJY8U2AeqX6CHWr1ji7OE17mX247b_IFi9kdGQS_aLdKSTLhv4KRTQaxQaOWo1Jpv2o', artistcount, 0, 'medium_term').then(function(data) {
+spotifyApi.getMyTopArtists('artists', 'BQA-VMlANGOMpR09g5nTMCTYtd5R78bpxueV08E8L5z20zge34gI8ujH8933Y2tsz7M2TDPyKf1oPp3wWsghIu2SIfyQEXAeOJY8U2AeqX6CHWr1ji7OE17mX247b_IFi9kdGQS_aLdKSTLhv4KRTQaxQaOWo1Jpv2o', artistcount, 0, 'medium_term', 'GB').then(function(data) {
         console.log('TOP TRACKS', data.body);
         }, function(err) {
             console.log('Something went wrong!', err);
